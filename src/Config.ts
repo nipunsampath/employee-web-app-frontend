4c93c25eff3861992b3839dbad2c80c2c07d5d85
Nipun Sampath <nipunsampath@gmail.com>
2020-08-13 04:17:39 +0530

    changed redirect URI

--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,6 @@
 export const config = {
     appId: '2f29763f-0a45-4183-8ee1-e2c18f93db6e',
-    // redirectUri: 'https://employeewebappstorage.z30.web.core.windows.net/',
-    redirectUri: 'http://localhost:3000',
+    redirectUri: 'https://black-dune-0dd9c8110.azurestaticapps.net/',
     scopes: [
       'user.read'
       
